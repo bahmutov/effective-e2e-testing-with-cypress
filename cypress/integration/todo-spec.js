d16ac74994fec47eec017f616708998975522f69
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-11 16:31:08 -0400

    only still runs two specs

--- cypress/integration/todo-spec.js
+++ cypress/integration/todo-spec.js
@@ -17,6 +17,34 @@ it('is focused on new item', () => {
   cy.focused().should('have.id', 'new-todo')
 })
 
+describe.only('TodoMVC main features', () => {
+  beforeEach(() => cy.visit(url))
+  it('adds and removes items', () => {
+    const todo = 'Learn Cypress'
+    cy.get('#new-todo').type(todo).type('{enter}')
+    cy.get('#new-todo').type(todo).type('{enter}')
+    cy.get('#new-todo').type(todo).type('{enter}')
+    cy.get('input.toggle').click({multiple: true})
+    cy.contains('Clear completed').click()
+  })
+})
+
+describe('TodoMVC main features with assertions', () => {
+  beforeEach(() => cy.visit(url))
+  it('adds and removes items', () => {
+    const todo = 'Learn Cypress'
+    cy.get('#new-todo').type(todo).type('{enter}')
+    cy.get('#new-todo').type(todo).type('{enter}')
+    cy.get('#new-todo').type(todo).type('{enter}')
+    cy.contains('#todo-count', 3)
+    cy.get('#todo-list li').should('have.length', 3)
+    cy.get('input.toggle').click({multiple: true})
+    cy.contains('#todo-count', 0)
+    cy.contains('Clear completed').click()
+    cy.get('#todo-list li').should('have.length', 0)
+  })
+})
+
 context('New Todo', () => {
   beforeEach(() => {
     cy.visit(url)
