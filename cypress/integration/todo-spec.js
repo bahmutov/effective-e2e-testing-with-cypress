b6df81d64c627601ff4553ab4087f2e6fc80e812
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-11 16:32:25 -0400

    remove all tests but the problem

--- cypress/integration/todo-spec.js
+++ cypress/integration/todo-spec.js
@@ -1,23 +1,6 @@
-// common frameworks
 const url = 'http://todomvc.com/examples/angularjs'
-// const url = 'http://todomvc.com/examples/angular2/'
 
-// Vue and Polymer uses different CSS names
-// .new-todo instead of #new-todo
-// .todo-list instead of #todo-list
-// const url = 'http://todomvc.com/examples/vue/'
-// const url = 'http://todomvc.com/examples/polymer/index.html'
-
-it('opens todo app', () => {
-  cy.visit(url)
-})
-
-it('is focused on new item', () => {
-  cy.visit(url)
-  cy.focused().should('have.id', 'new-todo')
-})
-
-describe.only('TodoMVC main features', () => {
+describe.only('TodoMVC main features', function () {
   beforeEach(() => cy.visit(url))
   it('adds and removes items', () => {
     const todo = 'Learn Cypress'
@@ -29,7 +12,7 @@ describe.only('TodoMVC main features', () => {
   })
 })
 
-describe('TodoMVC main features with assertions', () => {
+describe('TodoMVC main features with assertions', function () {
   beforeEach(() => cy.visit(url))
   it('adds and removes items', () => {
     const todo = 'Learn Cypress'
@@ -42,20 +25,5 @@ describe('TodoMVC main features with assertions', () => {
     cy.contains('#todo-count', 0)
     cy.contains('Clear completed').click()
     cy.get('#todo-list li').should('have.length', 0)
-  })
-})
-
-context('New Todo', () => {
-  beforeEach(() => {
-    cy.visit(url)
-  })
-
-  const firstTodo = 'Learn Cypress'
-
-  it('adds 1 todo', () => {
-    cy.get('#new-todo').type(firstTodo).type('{enter}')
-    cy.get('#todo-list li')
-      .first()
-      .find('label').should('contain', firstTodo)
   })
 })
