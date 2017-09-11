6e2ed1b9fe885702d8256762eb943e1f3ad2530f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-11 16:36:23 -0400

    minimal example

--- cypress/integration/todo-spec.js
+++ cypress/integration/todo-spec.js
@@ -1,29 +1,7 @@
-const url = 'http://todomvc.com/examples/angularjs'
-
-describe.only('TodoMVC main features', function () {
-  beforeEach(() => cy.visit(url))
-  it('adds and removes items', () => {
-    const todo = 'Learn Cypress'
-    cy.get('#new-todo').type(todo).type('{enter}')
-    cy.get('#new-todo').type(todo).type('{enter}')
-    cy.get('#new-todo').type(todo).type('{enter}')
-    cy.get('input.toggle').click({multiple: true})
-    cy.contains('Clear completed').click()
-  })
+describe.only('main features', function () {
+  it('first spec', function () {})
 })
 
-describe('TodoMVC main features with assertions', function () {
-  beforeEach(() => cy.visit(url))
-  it('adds and removes items', () => {
-    const todo = 'Learn Cypress'
-    cy.get('#new-todo').type(todo).type('{enter}')
-    cy.get('#new-todo').type(todo).type('{enter}')
-    cy.get('#new-todo').type(todo).type('{enter}')
-    cy.contains('#todo-count', 3)
-    cy.get('#todo-list li').should('have.length', 3)
-    cy.get('input.toggle').click({multiple: true})
-    cy.contains('#todo-count', 0)
-    cy.contains('Clear completed').click()
-    cy.get('#todo-list li').should('have.length', 0)
-  })
+describe('main features with assertions', function () {
+  it('second spec', function () {})
 })
