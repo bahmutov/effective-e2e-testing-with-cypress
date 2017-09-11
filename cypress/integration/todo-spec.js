6da9cc0f5d224c471b91c997f546016a51d08918
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-11 16:08:19 -0400

    type new todo

--- cypress/integration/todo-spec.js
+++ cypress/integration/todo-spec.js
@@ -7,3 +7,18 @@ it('is focused on new item', () => {
   cy.visit(url)
   cy.focused().should('have.class', 'new-todo')
 })
+
+context.only('New Todo', () => {
+  beforeEach(() => {
+    cy.visit(url)
+  })
+
+  const firstTodo = 'Learn Cypress'
+
+  it('adds 1 todo', () => {
+    cy.get('.new-todo').type(firstTodo).type('{enter}')
+    cy.get('.todo-list li')
+      .first()
+      .find('label').should('eq', firstTodo)
+  })
+})
