f6d4d6b835226432ef71d917966eac7648eee7ed
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-13 00:21:05 -0400

    more testing

--- cypress/integration/todo-spec.js
+++ cypress/integration/todo-spec.js
@@ -1,6 +1,6 @@
 // common frameworks
-// const url = 'http://todomvc.com/examples/angularjs'
-const url = 'http://todomvc.com/examples/angular2/'
+const url = 'http://todomvc.com/examples/angularjs'
+// const url = 'http://todomvc.com/examples/angular2/'
 // const url = 'https://blacksonic.github.io/angular2-esnext-todomvc/'
 
 // Vue and Polymer uses different CSS names
@@ -16,21 +16,36 @@ it('opens todo app', () => {
 
 it('is focused on new item', () => {
   cy.visit(url)
-  cy.focused().should('have.class', 'new-todo')
+  cy.focused().should('have.id', 'new-todo')
 })
 
-context('New Todo', () => {
+describe('New Todo', () => {
   beforeEach(() => {
     cy.visit(url)
   })
 
-  const firstTodo = 'Learn Cypress'
+  it('loads the page', () => {
+    cy.contains('h1', 'todos')
+  })
 
-  it.only('adds 1 todo', () => {
-    cy.get('.new-todo')
+  const firstTodo = 'Learn Cypress'
+  it('adds 1 todo', () => {
+    cy.get('#new-todo')
       .type(firstTodo)
       .type('{enter}')
-    cy.get('.todo-list li')
+    cy.get('#todo-list')
+      .find('li')
+      .first()
+      .find('label').should('contain', firstTodo)
+  })
+
+  it('adds 1 todo on iPhone', () => {
+    cy.viewport('iphone-6')
+    cy.get('#new-todo')
+      .type(firstTodo)
+      .type('{enter}')
+    cy.get('#todo-list')
+      .find('li')
       .first()
       .find('label').should('contain', firstTodo)
   })
